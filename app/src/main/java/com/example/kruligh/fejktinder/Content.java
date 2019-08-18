8c98d1043cee1fd989946d8c68c6bbaa8bf299ea
kruligh <kruligh@DESKTOP-V3J9PPG.localdomain>
2019-08-18 18:06:42 +0200

    First verse content

---
+++ app/src/main/java/com/example/kruligh/fejktinder/Content.java
@@ -0,0 +1,67 @@
+package com.example.kruligh.fejktinder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Content {
+
+    public Map<String, String> getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(Map<String, String> lyrics) {
+        this.lyrics = lyrics;
+    }
+
+    private Map<String, String> lyrics;
+
+    public Content() {
+        this.lyrics = new HashMap<>();
+        this.lyrics.put("1", "yoo \uD83D\uDC4A");
+        this.lyrics.put("3", "to ja \uD83D\uDC30");
+        this.lyrics.put("4", "ballada o białych paznokciach \uD83D\uDC85");
+        this.lyrics.put("6", "jest rap jest mózg \uD83E\uDDE0");
+        this.lyrics.put("7", "uczucia to nie obciach \uD83D\uDC93");
+        this.lyrics.put("9", "hehe");
+
+        this.lyrics.put("10", "Nagrywam to dla wszystkich wspaniałych dziewcząt \uD83D\uDE4B\u200D");
+        this.lyrics.put("13", "I dla wszystkich facetów którzy \uD83D\uDE4B\u200D");
+        this.lyrics.put("16", "Szukają swojej księżniczki \uD83D\uDC78");
+        this.lyrics.put("18", "bedzie dobrze ; )");
+
+        this.lyrics.put("21", "Krążę wokół Ciebie jak krążownik \uD83D\uDEF8 \uD83C\uDF0C \uD83D\uDC9E");
+        this.lyrics.put("23", "Ksiażke przeczytałem, nawet słownik \uD83D\uDCDA \uD83D\uDCDA");
+        this.lyrics.put("26", "Żeby odpisać Ci z przecinkiem, jak na Polskim \uD83D\uDCD1 \uD83D\uDCD5");
+        this.lyrics.put("28", "I wysyłam emotki takie jak Ty");
+        this.lyrics.put("30", "żeby sie upodobnić \uD83E\uDD13 \uD83D\uDD0E");
+
+        this.lyrics.put("32", "Potem chcę wino, to wytrawne, czerwone \uD83C\uDF77 \uD83C\uDF77 \uD83C\uDF77");
+        this.lyrics.put("34", "Okiem rzucę na Ciebie \uD83D\uDE0D");
+        this.lyrics.put("35", "wybiorę słodkie, różowe \uD83C\uDF79");
+        this.lyrics.put("38", "Mów o swoich bólach, lipa");
+        this.lyrics.put("39", "narzekaj");
+        this.lyrics.put("40", "głowa? \uD83D\uDE47\u200D");
+        this.lyrics.put("41", "W moich googlach, klikam - apteka całodobowa \uD83D\uDD0E \uD83D\uDE91 \uD83D\uDC8A");
+
+        this.lyrics.put("43", "Coś budzi we mnie to, że nie jesteś łatwa \uD83E\uDDD0");
+        this.lyrics.put("46", "Pewnie się spóźnię bo");
+        this.lyrics.put("47", "chciałem zdobyć \uD83C\uDF37 Cii \uD83D\uDC90");
+        this.lyrics.put("48", "I wtedy mówisz że, yyy ta");
+        this.lyrics.put("49", "ta koszula ładna \uD83D\uDC55");
+        this.lyrics.put("51", "I wiemy w czym wyskoczę");
+        this.lyrics.put("52", "żeby w weekend Cie gdzieś zabrać. \uD83D\uDE97 \uD83C\uDF07 \uD83C\uDFD6");
+
+        this.lyrics.put("55", "Pajacuje jak pajac za jeden Twój \uD83D\uDE42 \uD83E\uDD38\u200D");
+        this.lyrics.put("57", "Jak go nie widzę ranooo \uD83D\uDECC");
+        this.lyrics.put("58", "ja jebie \uD83E\uDD26\u200D");
+        this.lyrics.put("59", "prawie umre \uD83D\uDC80");
+        this.lyrics.put("60", "Bedę");
+        this.lyrics.put("61", "nieważne co by się działo");
+        this.lyrics.put("62", "❓Możesz malować dla mnie paznokcie na biało❓");
+
+        this.lyrics.put("64", "Bo mi się podobają");
+
+    }
+
+
+}
--- app/src/main/java/com/example/kruligh/fejktinder/NotificationService.java
+++ app/src/main/java/com/example/kruligh/fejktinder/NotificationService.java
@@ -12,6 +12,8 @@ import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import static java.lang.Math.toIntExact;
@@ -22,6 +24,8 @@ public class NotificationService extends IntentService {
 
     String channelName = "asd123";
     String channelId = "channel-id-tinder";
+    Content content = new Content();
+    int delay = 50 * 1000;
 
     private MediaPlayer audioPlayer = null;
 
@@ -60,10 +64,11 @@ public class NotificationService extends IntentService {
     protected void onHandleIntent(Intent workIntent) {
         long startTime = new Date().getTime();
         this.initAudioPlayer();
+        this.audioPlayer.seekTo(delay);
         this.audioPlayer.start();
 
-        for (long nowSec = 0; nowSec < 30;) {
-            long nowMili = new Date().getTime() - startTime;
+        for (long nowSec = 0; nowSec < 70;) {
+            long nowMili = delay + new Date().getTime() - startTime;
             nowSec = nowMili / 1000;
 
             try {
@@ -81,12 +86,13 @@ public class NotificationService extends IntentService {
     }
 
     private void handleSecond(long nowSec) {
-       showTextNotification(toIntExact(nowSec),"" + nowSec);
+       showTextNotification(toIntExact(nowSec), content.getLyrics().get("" + nowSec));
     }
 
-
     private void showTextNotification(int nowSec, String message) {
-
+        if(message == null) {
+            return;
+        }
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.mes2, 0)
 //                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.drawable.mes2))
@@ -106,236 +112,5 @@ public class NotificationService extends IntentService {
         if (nowSec  > 50) {
             notificationManager.cancel(nowSec - 50);
         }
-    }
-
-    private String getCategory(int i) {
-        switch (i % 10) {
-            case 0:
-                return NotificationCompat.CATEGORY_ALARM;
-            case 1:
-                return NotificationCompat.CATEGORY_CALL;
-            case 2:
-                return NotificationCompat.CATEGORY_EMAIL;
-            case 3:
-                return NotificationCompat.CATEGORY_ERROR;
-            case 4:
-                return NotificationCompat.CATEGORY_EVENT;
-            case 5:
-                return NotificationCompat.CATEGORY_MESSAGE;
-            case 6:
-                return NotificationCompat.CATEGORY_PROGRESS;
-            case 7:
-                return NotificationCompat.CATEGORY_SOCIAL;
-            case 8:
-                return NotificationCompat.CATEGORY_STATUS;
-            case 9:
-                return NotificationCompat.CATEGORY_REMINDER;
-            default:
-                return NotificationCompat.CATEGORY_STATUS;
-        }
-    }
-
-    private String getContent(int i) {
-        String name = getName(i);
-        String[] proposals = {
-                "Użytkownik " + name + " wysłał Ci nową wiadomość.", "Użytkownik " + name + " wysłał Ci nową wiadomość.",
-                "Użytkownik " + name + " wysłał Ci nową wiadomość.", //3
-                name + " wysłał(a) Ci Super Like!",
-                "Masz nową parę!", "Masz nową parę!",
-                "Masz nową parę!", "Masz nową parę!",
-                "Masz nową parę!", "Masz nową parę!", // 7
-        };
-
-        return proposals[getRandom(0, 9)];
-    }
-
-    private String getName(int i) {
-        if (i == 20) {
-            return "Twoja stara";
-        }
-        String[] names = {
-                "Basia",
-                "Asia",
-                "Gabrysia",
-//                "Twoja stara",
-                "Camila",
-                "Roksi",
-                "Kasia",
-                "Katarzyna",
-                "Julia",
-                "Zuzanna",
-                "Zofia",
-                "Lena",
-                "Maja",
-                "Hanna",
-                "Amelia",
-                "Alicja",
-                "Maria",
-                "Aleksandra",
-                "Oliwia",
-                "Natalia",
-                "Wiktoria",
-                "Emilia",
-                "Antonina",
-                "Laura",
-                "Pola",
-                "Iga",
-                "Anna",
-                "Liliana",
-                "Zuzanna",
-                "Lena",
-                "Julia",
-                "Maja",
-                "Zofia",
-                "Alicja",
-                "Nati",
-                "Nikola",
-                "Anna",
-                "Antonina",
-                "Nadia",
-                "Gabriela",
-                "Julia",
-                "Zuzanna",
-                "Zofia",
-                "Lena",
-                "Maja",
-                "Hanna",
-                "Amelia",
-                "Alicja",
-                "Maria",
-                "Aleksandra",
-                "Oliwia",
-                "Natalia",
-                "Wiktoria",
-                "Wiki",
-                "Emilia",
-                "Antonina",
-                "Laura",
-                "Pola",
-                "Iga",
-                "Anna",
-                "Liliana",
-                "Nadia",
-                "Marcelina",
-                "Gabriela",
-                "Michalina",
-                "Kornelia",
-                "Nikola",
-                "Helena",
-                "Milena",
-                "Martyna",
-                "Jagoda",
-                "Karolina",
-                "Agata",
-                "Nina",
-                "Magdalena",
-                "Barbara",
-                "Weronika",
-                "Blanka",
-                "Łucja",
-                "Kaja",
-                "Klara",
-                "Anastazja",
-                "Kinga",
-                "Nela",
-                "Mela",
-                "Joanna",
-                "Małgorzata",
-                "Lilianna",
-                "Paulina",
-                "Sara",
-                "Izabela",
-                "Marta",
-                "Matylda",
-                "Katarzyna",
-                "Aniela",
-                "Róża",
-                "Klaudia",
-                "Ewa",
-                "Eliza",
-                "Patrycja",
-                "Dominika",
-                "Kalina",
-                "Adrianna",
-                "Urszula",
-                "Rozalia",
-                "Rose",
-                "Lidia",
-                "Olga",
-                "Melania",
-                "Malwina",
-                "Liwia",
-                "Bianka",
-                "Daria",
-                "Kamila",
-                "Aurelia",
-                "Dorota",
-                "Sandra",
-                "Mia",
-                "Marika",
-                "Ada",
-                "Dagmara",
-                "Roksana",
-                "Jowita",
-                "Ida",
-                "Karina",
-                "Inga",
-                "Olivia",
-                "Agnieszka",
-                "Apolonia",
-                "Elena",
-                "Gaja",
-                "Luiza",
-                "Monika",
-                "Justyna",
-                "Diana",
-                "Dianka",
-                "Natasza",
-                "Tola",
-                "Emma",
-                "Jadwiga",
-                "Julita",
-                "Nel",
-                "Vanessa",
-                "Rita",
-                "Marlena",
-                "Victoria",
-                "Elżbieta",
-                "Ela",
-                "Nicola",
-                "Lilia",
-                "Anita",
-                "Adela",
-                "Ewelina",
-                "Sonia",
-                "Alina",
-                "Stefania",
-                "Celina",
-                "Otylia",
-                "Lea",
-                "Lila",
-                "Sabina",
-                "Jaśmina",
-                "Konstancja",
-                "Julianna",
-                "Sofia",
-                "Krystyna",
-                "Judyta",
-                "Maya",
-                "Amanda",
-                "Faustyna",
-                "Teresa",
-                "Jessica",
-                "Izabella",
-                "Mila",
-                "Sylwia",
-                "Noemi",
-                "Paula",
-        };
-        return names[getRandom(0, names.length - 1)];
-    }
-
-    private int getRandom(int min, int max) {
-        return rand.nextInt(max - min + 1) + min;
     }
 }
